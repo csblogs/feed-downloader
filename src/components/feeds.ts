e1ee882cf6194d368b29890fbf3e3aa0ac6b31cd
Daniel Brown <d.t.brown@outlook.com>
2019-11-17 22:26:51 +0000

    Implemented `getAllFeeds`

--- src/components/feeds.ts
+++ src/components/feeds.ts
@@ -1,3 +1,4 @@
+import { query } from '../database';
 import { PoolClient } from 'pg';
 import { IBlogPost } from './blog-posts';
 
@@ -13,11 +14,17 @@ export interface IFeedDownload extends IFeed {
 }
 
 export async function getAllFeeds(): Promise<IFeed[]> {
-  return [{
-    id: '1',
-    url: 'https://dannycomputerscientist.wordpress.com/feed.xml', 
-    lastModified: new Date()
-  }];
+  const { rows } = await query('SELECT id, url, lastModified FROM users', null);
+  
+  const feeds: IFeed[] = rows.map(row => {
+    return {
+      id: row.id,
+      url: row.blog_feed_uri,
+      lastModified: new Date(row.feed_last_modified)
+    }
+  });
+
+  return feeds;
 }
 
 export async function downloadFeedIfModified(feed: IFeed): Promise<IFeedDownload> {
